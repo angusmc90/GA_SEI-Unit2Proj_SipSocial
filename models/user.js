fae55b5e9e650aad6398afe5627335a36167dbd7
angusmc90 <angusmc90@gmail.com>
2023-10-31 23:43:12 -0500

    create users model

--- models/user.js
+++ models/user.js
@@ -1,3 +1,26 @@
 const mongoose = require('mongoose');
 
-// Create your User Model
+
+const sipsUserSchema = new mongoose.Schema({
+    // Creating googleID first
+    // There won't be any log-ins without OAuth
+    googleId: {
+        type: String,
+        required: true
+      },
+    firstName: String,
+    lastName: String,
+    profileName: String,
+    profilePic: String,
+    location: String,
+    favDrink: String,
+    hateDrink: String,
+    DOB: Date,
+    aboutMe: String,
+    drinks: Array
+}, {
+    timstamps: true
+});
+
+
+module.exports = mongoose.model('SipSUser', sipsUserSchema)
