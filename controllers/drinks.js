6de663638337724ba839f1e538ebbb7e7d4ce28a
angusmc90 <angusmc90@gmail.com>
2023-11-06 02:37:21 -0600

    add more functions to drinks model

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -3,12 +3,13 @@ const UserModel = require('../models/users')
 
 module.exports = {
     new: newDrink, // take to the page to create an order
-    create, // create the new 
-    index, // load all drinks
+    create, // create a new drink doc
+    index, // load all drinks at the-bar
     show, // read someone's post in the-hightop
     deleteDrink, // delete the user's post
 }
 
+// read someone's post in the-hightop
 async function show (req, res) {
     console.log('ctrlDrnkShow');
     try {
@@ -24,6 +25,38 @@ async function show (req, res) {
     }
 }
 
+// load all drinks at the-bar
+async function index (req, res) {
+    console.log('index controler')
+    try {
+        const drinkDocuments = await DrinkModel.find([{}]);
+        console.log('drink docs')
+        res.render('drinks/index', { drinkDocs: drinkDocuments});
+    } catch (err) {
+        console.log(err)
+        res.send(err);
+    }
+}
+
+// take to the page to create an order
 function newDrink (req, res) {
     res.render('/the-bartender')
+}
+
+// create a new drink doc
+async function create (req, res, next) {
+    console.log(req.body, '| FORM CONTENTS');
+    console.log(req.user, '| USER DETAILS');
+
+    //need to change the value of the checkbox to a boolean
+    req.body.orderAgainTF = !!req.body.orderAgainTF;
+    console.log(req.body);
+
+    try {
+        const drinkDoc = await DrinkModel.create(req.body);
+        console.log(drinkDoc, '| THE DOC JUST CREATED')
+    } catch (er) {
+        console.log(err)
+        res.send(err);
+    }
 }
